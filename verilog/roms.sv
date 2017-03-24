9f7f4e85d347fd86eb9217a421ddc023027a1f46
sear-PC\sear <peterlimail47@gmail.com>
2017-03-24 15:28:19 -0700

    Began working on NROM awaiting response from nesdev forums

--- verilog/roms.sv
+++ verilog/roms.sv
@@ -1,10 +1,25 @@
 /*
-	soms.sv
+	roms.sv
+	
+	Current version of rom implements something called 16k NROM 
+	Support for cooler versions of nes games is not implemented and also quite unlikely
+	
+	the test ROM that we will be using for this project is the american release of 
+	Donkey Kong it uses NROM.
+	
+	
+	
+	
 	static program ROM (to be loaded before startup of CPU)
+	NROM has the following characteristics 
+	program ROM at C000 to FFFF 
+	mirrored ROM at 8000 to BFFF
+	ram at $6000 to $7FFF (ram's not that big but it is mirrored across this region)
 */
 
 
 // Rom resides at C000 (1100 0 0 0 ) to FFFF in CPU address AND DMA address space
+// This is also mirrored to $8000 to BFFF
 module prg_rom(input logic [13:0]AB, // 
 	input logic CS);
 endmodule
