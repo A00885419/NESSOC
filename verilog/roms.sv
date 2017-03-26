0c6d187cf95b182d43f307e20543d3f5f9987dd2
peter li <peterlimail47@gmail.com>
2017-03-26 03:26:07 -0700

    Roms testing completed in accordance with rom_tb.sv  Next module: serial programming port

--- verilog/roms.sv
+++ verilog/roms.sv
@@ -22,56 +22,59 @@
 
 // Rom resides at C000 (1100 0 0 0 ) to FFFF in CPU address AND DMA address space
 // This is also mirrored to $8000 to BFFF
-`define PRG_ROM_LOWER 'h8000 // lower 16k of prg Rom
-`define PRG_ROM_UPPER 'hC000 // Upper 16k
-`define PRG_ROM_END 'h10000  // ROM END 
-`define FILE_HEADER_END 'h0010 // end of nes header file 
-`define CHR_ROM_LOWER //lower 4k of chr Rom 
+
 module rom_master(
 	// CPU Access bus
 	input logic cpu_clk,
 	input logic [15:0]cpu_ab,
 	output logic [7:0]cpu_do,
 	// PPU access bus 
-	input logic ppu_clk, ppu_cs,
+	input logic ppu_clk,
 	input logic [15:0]ppu_ab,
 	output logic [7:0]ppu_do
+	//,
+	// RS232 Serial Port for programming 
+	// TODO make the interface for this later..
+	/*
+	input logic clk, din, prg_ctrl
+	*/
+	
 );
 
 logic [15:0]ppu_ptr;
 logic [15:0]cpu_ptr;
 
-logic [7:0]rom['h600f:0]// size of INES file, NROM style files with 16k Rom and 
-						// 8k CHR Rom is supported only, total romdump size is 24k
+logic [7:0]rom['h8000:0]; // size of INES file, NROM style files with 16k Rom and 
+						  // 8k CHR Rom is supported only, total romdump size is 24k
 initial begin 
 	// Gotta automate this somehow later. but for now , CHAAARGEE!
 	// nestest.nes can be found on  
 	// https://wiki.nesdev.com/w/index.php/Emulator_tests
-	$readmemb("nestest.nes",rom); 
+	$readmemh("hexdump.nes", rom); 
 end 
 
 // -------------CPU Access and decode-------------
 always_comb begin 
 	// 16kBits =  xxaa aaaa aaaa aaaa 
-	if(cpu_ab >= PRG_ROM_LOWER)	// no check for upper bound due to roll-over :3
-		cpu_ptr = cpu_ab[12:0] + FILE_HEADER_END;  
+	if(cpu_ab >= 'h8000)	// no check for upper bound due to roll-over :3
+		cpu_ptr = {cpu_ab[13:0]} + 'h0010;  
 	else cpu_ptr = 0;
 end
 
 always_ff@(posedge cpu_clk) begin
-	cpu_do = cpu_ptr[cpu_ptr];
+	cpu_do = rom[cpu_ptr];
 end 
 
 // -------------PPU Access and decode-------------
 always_comb begin 
 	// 8kBits =  xxxa aaaa aaaa aaaa 
-	if(ppu_ab >= PRG_ROM_LOWER)	// no check for upper bound due to roll-over :3
-		ppu_ptr = cpu_ab[11:0] + FILE_HEADER_END + 'h4000;
+	if(ppu_ab < 'h8000)	// no check for lower bound due to roll-over :3
+		ppu_ptr = {ppu_ab[11:0]} + 'h0010 + 'h4000;
 	else ppu_ptr = 0;
 end
 
 always_ff@(posedge ppu_clk) begin
-	ppu_do = ppu_ptr[cpu_ptr];
+	ppu_do = rom[ppu_ptr];
 end
 
 endmodule
--- verilog/testbenches/rom_tb.sv
+++ verilog/testbenches/rom_tb.sv
@@ -11,35 +11,27 @@ module rom_tb();
 	logic [7:0]ppu_do;
 	
 	integer i = 0;
-	
 	assign cpu_clk = clk;
 	assign ppu_clk = clk;
 	
-	always begin 
-		#20ns
-		cpu_clk = ~cpu_clk;
-		ppu_clk = ~ppu_clk;
-	end 
 	rom_master rom_dut(.*);
 	
 	initial begin 
-		$readmemh("")
-		cpu_clk = 0;
-		ppu_clk = 0;
+		clk = 0;
 		// test CHR Rom ppu $0000 -> $2000
 		// test PRG Rom (inc. mirror) $C000 -> $FFFF
 		//===============CPU TEST ================
 		//===========RST vector test==============
 		cpu_ab = 'hFFFC;
-		#21ns;
-		$display( "lower rst vector :%x, exp: 07", cpu_do);
+		#210ns;
+		$display( "lower rst vector :%x, exp: 04", cpu_do);
 		cpu_ab = 'hFFFC+1;	// test reset vector
-		#20ns;
+		#200ns;
 		$display( "upper rst vector :%x exp: C0", cpu_do);
 		//===========FIRST instruction test ======
-		cpu_ab = 'hC007;
-		#20ns;
-		$display( "FIRST vector :%x exp:SEI (78)", cpu_do);	
+		cpu_ab = 'hC004;
+		#200ns;
+		$display( "FIRST vector :%x exp: SEI (78)", cpu_do);	
 		//========== END OF CPU TEST =============
 		
 		//===============PPU TEST ================
@@ -47,12 +39,15 @@ module rom_tb();
 		$display("pm\t fm\t v"); //pm = ppu memory, fm= file memory, 
 		for (i =0; i<64; i++) begin  // Don't need to read all of em 
 			ppu_ab = i;
-			#20;
+			#200ns;
 			$display("%x\t %x\t %x", ppu_ab, ppu_ab+'h4010, ppu_do);
 		end
 		//========== END OF PPU TEST =============
-		$stop
+		$stop;
 	end 
 	
-
+	always begin 
+		#100ns;
+		clk = ~clk;
+	end 
 endmodule
